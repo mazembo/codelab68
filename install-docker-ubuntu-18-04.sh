ffa47cafb4b43210d5d565b1d312c46179eed58e
mazembo <mazemb_eddy@yahoo.fr>
2018-10-20 19:38:00 +0200

    run script as root

--- install-docker-ubuntu-18-04.sh
+++ install-docker-ubuntu-18-04.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+# run this script as root
+apt update
+apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update
-sudo apt install docker-ce
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt update
+apt install docker-ce
 curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
